6f83621df95ed139ec9ade9982b3fd5f8b3439e5
Waxer59 <hnboyano@gmail.com>
2023-01-09 17:33:11 +0100

    removed react router

--- src/editor/components/Editor.jsx
+++ src/editor/components/Editor.jsx
@@ -1,13 +1,11 @@
 import { useRef, useState, useEffect } from 'react';
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
-import { useRouteUrl } from '../hooks/useRouteUrl';
 import { useCodeStore } from '../hooks/useCodeStore';
 import { useSettingsStore } from '../hooks/useSettingsStore';
 import { useLocalStorage } from '../hooks/useLocalStorage';
 
 export const Editor = () => {
   const [editor, setEditor] = useState(null);
-  const { decodeText, saveCodeUrl } = useRouteUrl();
   const { onSetActiveCode, uploadedCode, activeCode } = useCodeStore();
   const { onSetSettings } = useSettingsStore();
   const { getLocalStorageItem } = useLocalStorage();
@@ -18,7 +16,7 @@ export const Editor = () => {
     if (monacoEl.current && !editor) {
       setEditor(
         monaco.editor.create(monacoEl.current, {
-          value: decodeText(),
+          value: /*! Change dynamic save */ '',
           language: 'javascript',
           automaticLayout: true, // resize the code area
           padding: {
@@ -45,12 +43,6 @@ export const Editor = () => {
   useEffect(() => {
     editor?.updateOptions(settings);
   }, [settings]);
-
-  useEffect(() => {
-    if (activeCode !== null) {
-      saveCodeUrl(activeCode);
-    }
-  }, [activeCode]);
 
   useEffect(() => {
     const settings = getLocalStorageItem('settings');
--- src/editor/pages/EditorPage.jsx
+++ src/editor/pages/EditorPage.jsx
@@ -1,10 +1,10 @@
+import { useEffect } from 'react';
 import { CodeEditor } from '../components/CodeEditor';
 import { SideBar } from '../components/SideBar';
 import { useRouteUrl } from '../hooks/useRouteUrl';
 import { useCodeStore } from '../hooks/useCodeStore';
 import '../helpers/userWorker';
 import '../helpers/editorSnippets';
-import { useEffect } from 'react';
 
 export const EditorPage = () => {
   const { onSetUploadedCode } = useCodeStore();
